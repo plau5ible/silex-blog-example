5b05ad297d03236b7d9744390901021bd8f74aab
Roma Lapin <roman.lapin@gmail.com>
2014-10-06 22:09:57 +0400

    security for admin

--- src/app.php
+++ src/app.php
@@ -26,6 +26,20 @@ $app->register(new Silex\Provider\TranslationServiceProvider(), array(
     'translator.domains' => array(),
 ));
 
+$app->register(new Silex\Provider\SecurityServiceProvider(), array(
+
+));
+$app['security.firewalls'] = array(
+    'admin' => array(
+        'pattern' => '^/admin',
+        'http' => true,
+        'users' => array(
+            // raw password is foo
+            'admin' => array('ROLE_ADMIN', '5FZ2Z8QIkA7UTZ4BYkoC+GsReLf569mSKDsfods6LYQ8t+a8EW9oaircfMpmaLbPBh4FOBiiFyLfuZmTSUwzZg=='),
+        ),
+    ),
+);
+
 $app->get('/', function() use ($app) {
     $sql = "SELECT * FROM posts";
     $posts = $app['db']->fetchAll($sql);
@@ -74,7 +88,7 @@ $app->match('/admin/add', function() use ($app) {
         $data = $form->getData();
         
         // do something with the data
-        $sql = "INSERT ..."
+        $sql = "INSERT ...";
 
         // redirect somewhere
         return $app->redirect('/');
