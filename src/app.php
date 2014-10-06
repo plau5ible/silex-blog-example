98c139f33084720adb6854edbb2169fb42723ab5
Roma Lapin <roman.lapin@gmail.com>
2014-10-06 22:04:01 +0400

    create post form

--- src/app.php
+++ src/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Response;
+use Silex\Provider\FormServiceProvider;
 
 $app = new Silex\Application();
 $app['debug'] = true;
@@ -19,6 +20,11 @@ $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
         ),
     )
 );
+$app->register(new FormServiceProvider());
+$app->register(new Silex\Provider\ValidatorServiceProvider());
+$app->register(new Silex\Provider\TranslationServiceProvider(), array(
+    'translator.domains' => array(),
+));
 
 $app->get('/', function() use ($app) {
     $sql = "SELECT * FROM posts";
@@ -30,19 +36,53 @@ $app->get('/', function() use ($app) {
 });
 
 $app->get('/post/{id}', function($id) use ($app) {
-    $sql = "SELECT * FROM posts WHERE id = :id";
+    $sql = "
+        SELECT * 
+        FROM posts p
+        WHERE p.id = :id
+    ";
     $post = $app['db']->fetchAssoc($sql, [
+        ':id' => $id,
+    ]);
+
+    $sql = "
+        SELECT * 
+        FROM comments c
+        WHERE c.post_id = :id
+    ";
+    $comments = $app['db']->fetchAll($sql, [
         ':id' => $id,
     ]);
 
     return $app['twig']->render('post.html.twig', [
         'post' => $post,
+        'comments' => $comments,
     ]);
 });
 
 
-$app->get('/admin/add', function() use ($app) {
-    return '';
+$app->match('/admin/add', function() use ($app) {
+    $form = $app['form.factory']->createBuilder('form')
+        ->add('title')
+        ->add('content', 'textarea')
+        ->getForm();
+    
+
+    $form->handleRequest($app['request']);
+
+    if ($form->isValid()) {
+        $data = $form->getData();
+        
+        // do something with the data
+        $sql = "INSERT ..."
+
+        // redirect somewhere
+        return $app->redirect('/');
+    }
+    return $app['twig']->render(
+        'add.html.twig', [
+            'form' => $form->createView()
+        ]);
 });
 
 $app->get('/admin/edit/{id}', function($id) use ($app) {
