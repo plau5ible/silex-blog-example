a0933673900397f6b84a6d14ef4181204157bbb8
Roma Lapin <roman.lapin@gmail.com>
2014-10-06 20:27:17 +0400

    url structure

--- src/app.php
+++ src/app.php
@@ -5,4 +5,38 @@ use Symfony\Component\HttpFoundation\Response;
 $app = new Silex\Application();
 $app['debug'] = true;
 
+$app->get('/', function() use ($app) {
+    return 'Главная';
+});
+
+$app->get('/post/{id}', function($id) use ($app) {
+    return 'Пост';
+});
+
+
+$app->get('/admin/add', function() use ($app) {
+    return '';
+});
+
+$app->get('/admin/edit/{id}', function($id) use ($app) {
+    return '';
+});
+
+$app->get('/admin/delete/{id}/', function($id) use ($app) {
+    return '';
+});
+
+$app->get('/add-comment', function() use ($app) {
+    return '';
+});
+
+$app->get('/edit-comment/{id}', function($id) use ($app) {
+    return '';
+});
+
+$app->get('/delete-comment/{id}', function($id) use ($app) {
+    return '';
+});
+
+
 return $app;
